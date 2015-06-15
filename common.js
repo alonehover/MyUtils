034191e661f8ed0b6d63232ecc42c4ace7d11c06
Pu Hongwu <puhongwu@live.com>
2015-06-15 14:26:32 +0800

    update

--- common.js
+++ common.js
@@ -59,3 +59,57 @@ var objectCreate = function (arg) {
 	return new obj;
 };
 Object.create = Object.create || objectCreate;
+
+
+/**
+ * js数组去重方法 1
+ */
+Array.prototype.unique3 = function(){
+	var res = [];
+	var json = {};
+	for(var i = 0; i < this.length; i++){
+		if(!json[this[i]]){
+			res.push(this[i]);
+			json[this[i]] = 1;
+		}
+	}
+	return res;
+}
+var arr = [112,112,34,'你好',112,112,34,'你好','str','str1'];
+alert(arr.unique3());
+
+/**
+ * indexOf 老浏览器兼容
+ */
+var indexOf = [].indexOf ?
+    function(arr, item) {
+      return arr.indexOf(item)
+    } :
+    function indexOf(arr, item) {
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+          return i
+        }
+      }
+      return -1
+    }
+
+/**
+ * 判断横竖屏
+ */ 
+function hengshuping(){  
+  if(window.orientation==180||window.orientation==0){  
+        alert("竖屏状态！");    
+   }  
+if(window.orientation==90||window.orientation==-90){  
+        alert("横屏状态！");         
+    }  
+ }  
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false); 
+
+/**
+ * 判断是否是从主屏幕图标进入的网站
+ */
+if ('standalone' in navigator && !navigator.standalone && (/iphone|ipod|ipad/gi).test(navigator.platform) && (/Safari/i).test(navigator.appVersion) && window.orientation==90||window.orientation==-90) {
+
+ }
