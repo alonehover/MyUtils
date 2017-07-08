bbe67a81f807b58e007b199f1bfc4f7e0ae6b351
alonehover <alonehover@qq.com>
2017-07-09 00:35:18 +0800

    tab

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,3 +1,16 @@
-function load() {
+window.onload = function() {
     window.document.designMode = "On";
+    const editor = document.getElementById("editor");
+
+    editor.addEventListener("keydown", function(e) {
+        if(e.keyCode == 9) {
+            e.preventDefault();
+            const indent = "\u00a0\u00a0\u00a0\u00a0";
+            const start = this.selectionStart;
+            const end = this.selectionEnd;
+            let selected = window.getSelection().toString();
+            selected = indent + selected.replace(/\n/g, "\n" + indent);
+            document.execCommand("insertHTML", false, selected)
+        }
+    });
 }
